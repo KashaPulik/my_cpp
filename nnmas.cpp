b6abd2a70a59a14b79e95ec6f262ce1a3274a99a
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-07-05 23:48:28 +0700

    aboba

--- nnmas.cpp
+++ nnmas.cpp
@@ -34,6 +34,44 @@ int* a_init(int** matrix, int* arr, int size)
     return arr;
 }
 
+int* b_init(int** matrix, int* arr, int size)
+{
+    int n = 0;
+    int i, j, g = 0;
+    while (n < size) {
+        j = 0;
+        i = n++;
+        while (i >= 0)
+            arr[g++] = matrix[i--][j++];
+    }
+    n = 1;
+    while (n < size) {
+        i = size - 1;
+        j = n++;
+        while (j < size)
+            arr[g++] = matrix[i--][j++];
+    }
+    return arr;
+}
+
+int* c_init(int** matrix, int* arr, int size)
+{
+    int center = size % 2 == 0 ? size / 2 - 1 : size / 2;
+    int n = size - 1;
+    int b = size - center;
+    int g = 0;
+    int i = center, j = center;
+    while (g < size * size) {
+        while (i > center)
+            arr[g++] = matrix[i--][j];
+        while (j < b)
+            arr[g++] = matrix[i][j++];
+        while (i < b)
+            arr[g++] = matrix[i++][j];
+        
+    }
+}
+
 void print_matrix(int** matrix, int size)
 {
     for(int i = 0; i < size; i++) {
@@ -53,10 +91,10 @@ void print_arr(int* arr, int size)
 
 int main()
 {
-    int size = 3;
+    int size = 4;
     int** matrix = create_matrix(size, 10);
     print_matrix(matrix, size);
     int* arr = new int[size * size];
-    a_init(matrix, arr, size);
+    b_init(matrix, arr, size);
     print_arr(arr, size * size);
 }
